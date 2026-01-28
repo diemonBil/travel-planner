0f182e378a7afd560f635d947db431b06b149a6f
Dmytro Bilopolskyi <dmytrobilopolskyi@gmail.com>
2026-01-28 13:15:00 +0200

    Rewrited serializers

--- main/travels/serializers.py
+++ main/travels/serializers.py
@@ -2,15 +2,18 @@ from rest_framework import serializers
 from .models import TravelProject, ProjectPlace
 
 
-class TravelProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TravelProject
-        fields = ['id', 'external_id', 'name', 'notes', 'visited', 'project']
-
-
 class ProjectPlaceSerializer(serializers.ModelSerializer):
-    places = TravelProjectSerializer(many=True, read_only=True)
 
     class Meta:
         model = ProjectPlace
-        fields = ['id', 'name', 'description', 'start_date', 'completed', 'places']
+        fields = ['id', 'external_id', 'title', 'notes', 'visited', 'project']
+
+
+class TravelProjectSerializer(serializers.ModelSerializer):
+    places = ProjectPlaceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = TravelProject
+        fields = ['id', 'name', 'description', 'start_date', 'is_completed', 'places']
+
+
